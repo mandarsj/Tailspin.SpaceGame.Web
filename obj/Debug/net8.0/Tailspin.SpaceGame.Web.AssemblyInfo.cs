d49a627b44d68a3cba53b6c3762eebbb39849356
Mandar Jogalekar <mandar.jogalekar@chanel.com>
2026-05-17 14:32:45 +0200

    update

--- obj/Debug/net8.0/Tailspin.SpaceGame.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tailspin.SpaceGame.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tailspin.SpaceGame.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a2c07eb7a701a061addb39290b900272a2f177f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa54e54a21f0e004584821b97741035af55395d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tailspin.SpaceGame.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tailspin.SpaceGame.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
